b7a8ca0b96e89f50e9564645d9b67fd1fbeed57e
Daniel Ribeiro <danielrocha05lol@gmail.com>
2023-11-06 18:57:52 -0500

    saving progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./components/Header";
-
+import Tasks from "./components/Tasks";
 import "./styles.css";
 const App = () => {
   return (
     <div>
       <Header />
+
+      <Tasks />
     </div>
   );
 };
